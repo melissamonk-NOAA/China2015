799566fbf96af6fcee6066fcc157c194a991f993
Melmonk <melissa.monk@noaa.gov>
2015-06-14 13:26:58 -0700

    Changed how and where the plots live

---
+++ Rcode/run_r4ss_mhm.R
@@ -0,0 +1,48 @@
+library(r4ss)
+#library(xtable)
+
+#setwd('C:/China2015')
+#dir.create(file.path(paste(getwd()), "plots_N"))
+#read in the SS north output file
+setwd('C:/Assessment2015/SS_chinaN')
+modN = SS_output(dir='C:/Assessment2015/SS_chinaN', forecast=T, covar=T, ncol=1000)
+save.image("Chinanorth2015.RData")
+SS_plots(modN,png=TRUE,html=FALSE,dir="C:/Assessment2015/SS_chinaN/")
+
+
+#read in the SS central output file
+setwd('C:/Assessment2015/SS_chinaC')
+modC = SS_output(dir='C:/Assessment2015/SS_chinaC', forecast=T, covar=T, ncol=1000)
+save.image("Chinacentral2015.RData")
+SS_plots(modC,png=TRUE,html=FALSE,dir="C:/Assessment2015/SS_chinaC/")
+
+#read in the SS south output file
+setwd('C:/Assessment2015/SS_chinaS')
+modS = SS_output(dir='C:/Assessment2015/SS_chinaS', forecast=T, covar=T, ncol=1000)
+save.image("Chinasouth2015.RData")
+SS_plots(modS,png=TRUE,html=FALSE,dir="C:/Assessment2015/SS_chinaS/")
+
+
+
+
+
+
+
+##################################################################################
+
+
+#load('Chinanorth2015.RData')
+#load('Chinasouth2015.Rdata')
+
+
+
+
+####write the entire myreplist to a file
+#sink("list_of_dataframes.csv", type="output")
+#invisible(lapply(modN, function(x) dput(write.csv(x))))
+#sink()
+
+
+#sink("mod_structure.csv", type="output")
+#invisible(str(modN,list.len = 9999))
+#sink()
